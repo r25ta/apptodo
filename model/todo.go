2941af8f385342d2c5cc848fb6b92d3c7720e09d
r25ta <twentyfive@hotmail.com>
2024-05-24 18:07:30 -0300

    First commit

---
+++ model/todo.go
@@ -0,0 +1,6 @@
+package model
+
+type Todo struct {
+	Id   int64
+	Item string
+}
---
+++ server.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	connStr := "postgresql://postgres:admin@localhost:5432/testdb?sslmode=disable"
+	//Connect to database
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	app.Static("/", "./public") // add this before starting the app
+
+	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+
+}
+
+type Todo struct {
+	Id   int64
+	Item string
+}
+
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	todos := make([]*Todo, 0)
+
+	rows, err := db.Query("SELECT id, item FROM todo")
+	defer rows.Close()
+
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON("Erro na execução da consulta no BD!")
+	}
+
+	for rows.Next() {
+		todo := new(Todo)
+
+		rows.Scan(&todo.Id, &todo.Item)
+
+		todos = append(todos, todo)
+	}
+
+	return c.Render("index", fiber.Map{"Todos": todos})
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+
+}
+
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+
+}
+
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello ")
+
+}
